8f9c74d67bd68a63c7d84daca2c3aa0e4a8e50ad
mono610 <mono703904@gmail.com>
2025-04-30 16:47:05 +0900

    雛形完成

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from deep-reinforcement-learning!")
+from envs.cartpole_env import make_env
+from utils.util import load_config
 
+def main():
+    config = load_config('configs/config.yaml')
+    env = make_env(config['env']['name'])
+    print('Environment name: ' + config['env']['name'])
+
+    obs, _ = env.reset()
+    for _ in range(10):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, _ = env.step(action)
+        if terminated or truncated:
+            print('episode done')
+            break
+    env.close()
 
 if __name__ == "__main__":
     main()
